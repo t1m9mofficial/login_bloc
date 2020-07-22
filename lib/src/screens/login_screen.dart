61e925dc538b166118a98fded6601cc0f456bfe6
Tahsin <t1m9m@Tahsins-MacBook-Pro.local>
2020-07-22 17:20:41 +0600

    first commit

---
+++ lib/src/screens/login_screen.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import '../blocs/bloc.dart';
+
+class LoginScreen extends StatelessWidget {
+  Widget build(context) {
+    return Container(
+      margin: EdgeInsets.all(20.0),
+      child: Column(
+        children: <Widget>[
+          emailField(),
+          passwrodField(),
+          Container(margin: EdgeInsets.only(top: 25.0)),
+          submitButton()
+        ],
+      ),
+    );
+  }
+
+  Widget emailField() {
+    return StreamBuilder(
+      stream: bloc.email,
+      builder: (context, snapshot) {
+        return TextField(
+          keyboardType: TextInputType.emailAddress,
+          decoration: InputDecoration(
+            hintText: 'you@example.com',
+            labelText: 'Email Address',
+            errorText: snapshot.error
+          ),
+          onChanged: bloc.changeEmail,
+        );
+      }
+    );
+  }
+
+  Widget passwrodField() {
+    return StreamBuilder(
+      stream: bloc.password,
+      builder: (context, snapshot) {
+        return TextField(
+          decoration: InputDecoration(
+            hintText: 'Password',
+            labelText: 'Password',
+            errorText: snapshot.error
+          ),
+          onChanged: bloc.changePassword,
+        );
+      }
+    );
+  }
+
+  Widget submitButton() {
+    return RaisedButton(
+      child: Text('Login'),
+      color: Colors.blue,
+      onPressed: () {},
+    );
+  }
+}
