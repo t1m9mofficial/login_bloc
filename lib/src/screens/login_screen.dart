9665a3b1a3e8fe8ddb35e0c45227101d015acf6c
Tahsin <t1m9m@Tahsins-MacBook-Pro.local>
2020-07-28 00:45:01 +0600

    Single scope provider

--- lib/src/screens/login_screen.dart
+++ lib/src/screens/login_screen.dart
@@ -1,22 +1,25 @@
 import 'package:flutter/material.dart';
 import '../blocs/bloc.dart';
+import '../blocs/provider.dart';
 
 class LoginScreen extends StatelessWidget {
   Widget build(context) {
+    final bloc = Provider.of(context);
+
     return Container(
       margin: EdgeInsets.all(20.0),
       child: Column(
         children: <Widget>[
-          emailField(),
-          passwrodField(),
+          emailField(bloc),
+          passwrodField(bloc),
           Container(margin: EdgeInsets.only(top: 25.0)),
-          submitButton()
+          submitButton(),
         ],
       ),
     );
   }
 
-  Widget emailField() {
+  Widget emailField(Bloc bloc) {
     return StreamBuilder(
       stream: bloc.email,
       builder: (context, snapshot) {
@@ -33,7 +36,7 @@ class LoginScreen extends StatelessWidget {
     );
   }
 
-  Widget passwrodField() {
+  Widget passwrodField(Bloc bloc) {
     return StreamBuilder(
       stream: bloc.password,
       builder: (context, snapshot) {
